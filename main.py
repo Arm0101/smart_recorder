745e1e85b1a7cfa81c57fce13185df4f73122650
Arm0101 <=>
2024-07-05 11:58:19 -0400

    test speaker classification

---
+++ main.py
@@ -0,0 +1,56 @@
+from utils import convert_audios_to_wav, preprocess_recording
+from dataset import extract_features, build_dataset
+import tensorflow as tf
+import numpy as np
+import os
+import librosa
+
+
+def rename_w_audio(path):
+    for file in os.listdir(path):
+        file_name = file.split(' ')[2:]
+        file_name = '_'.join(file_name)
+        name, ext = os.path.splitext(file_name)
+        file_name = name.replace('.', '-') + ext
+        file_name = os.path.join(path, file_name)
+        os.rename(os.path.join(path, file), file_name)
+
+
+def load_model(model_save_path):
+    if os.path.exists(model_save_path):
+        loaded_model = tf.keras.models.load_model(model_save_path)
+        return loaded_model
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    labels = ['carlos', 'jose', 'armando', 'ovidio']
+
+    # build_dataset('recordings', 'dataset')
+    # print('dataset completed')
+    test_paths = 'test_recordings'
+    convert_audios_to_wav(test_paths)
+    model = load_model('models/speaker_identification.keras')
+    if not model:
+        exit(1)
+    for audio_path in os.listdir(test_paths):
+        audio, sr = librosa.load(os.path.join(test_paths, audio_path))
+        # if librosa.get_duration(y=audio, sr=sr) > 11:
+        #     continue
+        full_path = person_path = os.path.join(test_paths, audio_path)
+        features = extract_features(full_path)
+
+        features = np.expand_dims(features, axis=0)  # Añadir dimensión de lote
+        features = np.expand_dims(features, axis=-1)  # Añadir dimensión de canal
+
+        prediction = model.predict(features)
+        print(f'prediction {prediction}\n')
+        predicted_label = np.argmax(prediction, axis=1)
+        print(f"Predicted label: {predicted_label}")
+        print(labels[predicted_label[0]])
+        print('-------------------------------\n')
+        # label_map_inverted = {v: k for k, v in label_map.items()}  # Invertir el mapa de etiquetas
+        # predicted_person = label_map_inverted[predicted_label[0]]
+
+    # print(f"Predicted person: {predicted_person}")
--- test2.py
+++ test2.py
@@ -7,7 +7,7 @@ from tensorflow.keras.layers import Conv1D, MaxPooling1D, LSTM, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
 from utils import ogg_to_wav
-from dataset import convert_audios_to_wav, prepare_dataset, extract_features
+from dataset import prepare_dataset, extract_features
 from tensorflow.keras.callbacks import EarlyStopping
 
 
@@ -26,8 +26,7 @@ dropout_rate = 0.4
 learning_rate = 0.0005
 
 # Ruta al directorio de los registros
-base_path = 'records'
-convert_audios_to_wav(base_path)
+base_path = 'dataset'
 data, labels, label_map = prepare_dataset(base_path)
 print(label_map)
 
@@ -39,15 +38,14 @@ print(f"Train size: {X_train.shape}")
 print(f"Validation size: {X_val.shape}")
 print(f"Test size: {X_test.shape}")
 
-# Reajustar los datos para que sean compatibles con la entrada de Conv1D y LSTM
+
 X_train = np.expand_dims(X_train, axis=-1)
 X_val = np.expand_dims(X_val, axis=-1)
 X_test = np.expand_dims(X_test, axis=-1)
 
-# Definir la nueva arquitectura de la red neuronal
 model = Sequential()
-model.add(Conv1D(filters=conv_filters, kernel_size=conv_kernel_size, strides=conv_stride, activation=conv_activation,
-                 input_shape=(X_train.shape[1], 1)))
+model.add(tf.keras.Input(shape=(X_train.shape[1], 1)))
+model.add(Conv1D(filters=conv_filters, kernel_size=conv_kernel_size, strides=conv_stride, activation=conv_activation))
 model.add(MaxPooling1D(pool_size=pool_size))
 model.add(Dropout(dropout_rate))
 
@@ -60,52 +58,22 @@ model.add(Dense(dense_units, activation='relu'))
 model.add(Dropout(dropout_rate))
 model.add(Dense(len(label_map), activation='softmax'))
 
-# Compilar el modelo
+
 model.compile(optimizer=Adam(learning_rate=learning_rate), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# Resumen del modelo
 model.summary()
 
 early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 
 # Entrenar el modelo con early stopping
-history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=100, batch_size=32)
+history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=50, batch_size=32)
 
 # Evaluar el modelo en el conjunto de prueba
 test_loss, test_accuracy = model.evaluate(X_test, y_test)
 print(f"Test Accuracy: {test_accuracy}")
 
 # Guardar el modelo en formato .keras
-model_save_path = './fine_tuned_speaker_identification.keras'
+model_save_path = 'models/speaker_identification.keras'
 model.save(model_save_path)
 
-# Verificar si el archivo existe antes de cargarlo
-if os.path.exists(model_save_path):
-    # Cargar el modelo guardado
-    loaded_model = tf.keras.models.load_model(model_save_path)
-    print("Modelo cargado exitosamente.")
-else:
-    print(f"El archivo {model_save_path} no existe. Por favor, verifica la ruta.")
 
-# Ejemplo de cómo preprocesar un nuevo archivo de audio para predicción
-input_audio = 'jose01.ogg'
-audio_path = 'jose01.wav'
-ogg_to_wav(input_audio, audio_path)
-
-features = extract_features(audio_path)
-
-# Reajustar las dimensiones para que coincidan con la entrada del modelo
-features = np.expand_dims(features, axis=0)  # Añadir dimensión de lote
-features = np.expand_dims(features, axis=-1)  # Añadir dimensión de canal
-
-# Realizar una predicción
-prediction = loaded_model.predict(features)
-print(f'prediction {prediction}\n')
-predicted_label = np.argmax(prediction, axis=1)
-
-# Mapear el índice de la etiqueta a la persona correspondiente
-label_map_inverted = {v: k for k, v in label_map.items()}  # Invertir el mapa de etiquetas
-predicted_person = label_map_inverted[predicted_label[0]]
-
-print(f"Predicted label: {predicted_label}")
-print(f"Predicted person: {predicted_person}")
